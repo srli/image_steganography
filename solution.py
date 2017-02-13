106d032b3a25f16f219f76e8194c1d5a6abfd324
srli <sophia.li@students.olin.edu>
2017-02-13 14:53:27 -0500

    template and solution set complete

--- solution.py
+++ solution.py
@@ -18,15 +18,16 @@ def decode_image(file_location="images/encoded_sample.png"):
 
     for i in range(x_size):
         for j in range(y_size):
-            pass #TODO: Fill in decoding functionality
-
+            if bin(red_channel.getpixel((i, j)))[-1] == '1':
+                pixels[i, j] = (255, 255, 255)
+            else:
+                pixels[i, j] = (0,0,0)
     decoded_image.save("images/decoded_image.png")
 
 def write_text(text_to_write, image_size):
     """Writes text to an RGB image. Automatically line wraps
 
     text_to_write: the text to write to the image
-    image_size: size of the resulting text image. Is a tuple (x_size, y_size)
     """
     image_text = Image.new("RGB", image_size)
     font = ImageFont.load_default().font
@@ -45,11 +46,35 @@ def encode_image(text_to_encode, template_image="images/samoyed.jpg"):
     text_to_encode: the text to encode into the template image
     template_image: the image to use for encoding. An image is provided by default.
     """
-    pass #TODO: Fill out this function
+    template_image = Image.open(template_image)
+    red_template = template_image.split()[0]
+    green_template = template_image.split()[1]
+    blue_template = template_image.split()[2]
+
+    x_size = template_image.size[0]
+    y_size = template_image.size[1]
+
+    #text draw
+    image_text = write_text(text_to_encode, template_image.size)
+    bw_encode = image_text.convert('1')
+
+    #encode text into image
+    encoded_image = Image.new("RGB", (x_size, y_size))
+    pixels = encoded_image.load()
+    for i in range(x_size):
+        for j in range(y_size):
+            red_template_pix = bin(red_template.getpixel((i,j)))
+            old_pix = red_template.getpixel((i,j))
+            tencode_pix = bin(bw_encode.getpixel((i,j)))
+
+            if tencode_pix[-1] == '1':
+                red_template_pix = red_template_pix[:-1] + '1'
+            else:
+                red_template_pix = red_template_pix[:-1] + '0'
+            pixels[i, j] = (int(red_template_pix, 2), green_template.getpixel((i,j)), blue_template.getpixel((i,j)))
+
+    encoded_image.save("images/encoded_image.png")
 
 if __name__ == '__main__':
-    print("Decoding the image...")
     decode_image()
-
-    print("Encoding the image...")
-    encode_image()
+    encode_image("hello world")
